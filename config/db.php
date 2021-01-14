4dcee45e09862e65f7af74ede597425fc69fb269
bill-zhuang <zstu_bill@yahoo.com>
2021-01-14 21:06:53 +0800

    add local db

--- config/db.php
+++ config/db.php
@@ -12,6 +12,13 @@ return [
         //'schemaCacheDuration' => 60,
         //'schemaCache' => 'cache',
     ],
+    'local_db' => [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=127.0.0.1;dbname=yii2',
+        'username' => 'root',
+        'password' => '123456',
+        'charset' => 'utf8mb4',
+    ],
     'testDB' => [
         'class' => 'yii\db\Connection',
         'dsn' => 'mysql:host=localhost;dbname=bill',
